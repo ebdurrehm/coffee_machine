d33576c256ac3560d4129bcf1512481ca308650e
Abdurrahim Ahmadov <42205442+ebdurrehm@users.noreply.github.com>
2020-06-26 21:12:10 +0400

    coffee machine version 1.1
    Let's simulate an actual coffee machine! What do we need for that? This coffee machine will have a limited supply of water, milk, coffee beans, and disposable cups. Also, it will calculate how much money it gets for selling coffee.
    
    There are several options for the coffee machine we want you to implement: first, it should sell coffee. It can make different types of coffee: espresso, latte, and cappuccino. Of course, each variety requires a different amount of supplies, however, in any case, you will need only one disposable cup for a drink. Second, the coffee machine must get replenished by a special worker. Third, another special worker should be able to take out money from the coffee machine.

--- machine.py
+++ machine.py
@@ -20,3 +20,75 @@ if cup_count>cup:
 else:
         print("No, I can make only {} cup(s) of coffee".format(cup_count))
     
+   
+
+class Coffee:
+    def __init__(self,water,milk,bean,cost,cup):
+        self.water=water
+        self.milk=milk
+        self.bean=bean
+        self.cost=cost
+        self.cup=cup
+        
+class Action(Coffee):
+    def __init__(self,water,milk,bean,cost,cup):
+        Coffee.__init__(self,water,milk,bean,cost,cup)
+    def buy(self):
+        chose=input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n")
+
+        if chose=='1':
+            self.water=self.water-250
+            self.bean=self.bean-16
+            self.cost=self.cost+4
+            self.cup-=1
+        if chose=='2':
+            self.water=self.water-350
+            self.milk=self.milk-75
+            self.bean=self.bean-20
+            self.cost=self.cost+7
+            self.cup -= 1
+        if chose=='3':
+            self.water=self.water-200
+            self.milk=self.milk-100
+            self.bean=self.bean-12
+            self.cost=self.cost+6
+            self.cup -= 1
+
+    def fill(self):
+        self.water=self.water+int(input("Write how many ml of water do you want to add:\n"))
+        self.milk=self.milk+int(input("Write how many ml of milk do you want to add:\n"))
+        self.bean=self.bean+int(input("Write how many grams of coffee beans do you want to add:\n"))
+        self.cup=self.cup+int(input("Write how many disposable cups of coffee do you want to add:\n"))
+
+    def  take(self):
+        print("I gave you {}".format(self.cost))
+        self.cost=0
+
+    def state(self):
+        print("The coffee machine has:\n {0} of water\n "
+              "{1} of milk\n {2} of coffee beans\n "
+              "{3} of disposable cups\n {4} of money".format(self.water,self.milk,self.bean,self.cup,self.cost))
+
+
+def action(machine):
+    action = input("Write action (buy, fill, take):\n")
+    if action == 'buy':
+        machine.buy()
+    if action == 'fill':
+        machine.fill()
+    if action == 'take':
+        machine.take()
+
+def main():
+    machine=Action(400 , 540, 120 , 550 , 9)
+    print(machine.state())
+    action(machine)
+    print(machine.state())
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
